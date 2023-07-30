66395c3cfec56bf4d368b5f18326880d3e99d953
shafayet1991 <shafayethossen1991@gmail.com>
2023-07-30 11:29:11 +0600

    controller segregated

---
+++ src/main/java/com/xyz/bd/webmaster/Modules/VTS/Controller/FuelLogController.java
@@ -0,0 +1,12 @@
+package com.xyz.bd.webmaster.Modules.VTS.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FuelLogController {
+
+}
---
+++ src/main/java/com/xyz/bd/webmaster/Modules/VTS/Controller/LogsController.java
@@ -0,0 +1,23 @@
+package com.xyz.bd.webmaster.Modules.VTS.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LogsController {
+    @RequestMapping(value="/vts/maintenance-log", method = RequestMethod.GET)
+    public ModelAndView showVtsMaintenanceLogView(ModelMap model){
+        model.addAttribute("title", "maintenance");
+        model.addAttribute("menu", "maintenance");
+        return new ModelAndView("iot-devices/vts/logs/vts-maintenance-log");
+    }
+    @RequestMapping(value="/vts/fuel-log", method = RequestMethod.GET)
+    public ModelAndView showVtsAlertView(ModelMap model){
+        model.addAttribute("title", "Map View");
+        model.addAttribute("menu", "VTS Trips");
+        return new ModelAndView("iot-devices/vts/logs/vts-fuel-log");
+    }
+}
