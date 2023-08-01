a0cbdc129ea4d1e29914899c864e34faca98df83
shafayet1991 <shafayethossen1991@gmail.com>
2023-08-01 21:47:19 +0600

    drivers pages UI done

---
+++ src/main/java/com/xyz/bd/webmaster/Modules/VTS/Controller/DriversController.java
@@ -0,0 +1,36 @@
+package com.xyz.bd.webmaster.Modules.VTS.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DriversController {
+    @RequestMapping(value="/vts/drivers-list", method = RequestMethod.GET)
+    public ModelAndView showDriverListPage(ModelMap model){
+        model.addAttribute("title", "VTS Drivers List");
+        model.addAttribute("menu", "VTS Drivers");
+        return new ModelAndView("iot-devices/vts/drivers/drivers");
+    }
+    @RequestMapping(value="/vts/add-driver-info", method = RequestMethod.GET)
+    public ModelAndView showAddDriverInfoFormPage(ModelMap model){
+        model.addAttribute("title", "VTS Drivers");
+        model.addAttribute("menu", "VTS Drivers");
+        return new ModelAndView("iot-devices/vts/drivers/add-driver-info");
+    }
+    @RequestMapping(value="/vts/add-driver-license", method = RequestMethod.GET)
+    public ModelAndView showAddDriverlicenseFormPage(ModelMap model){
+        model.addAttribute("title", "VTS Drivers");
+        model.addAttribute("menu", "VTS Drivers");
+        return new ModelAndView("iot-devices/vts/drivers/add-driver-license");
+    }
+
+    @RequestMapping(value="/vts/add-driver-additional-info", method = RequestMethod.GET)
+    public ModelAndView showAddDriverAdditionalInfoFormPage(ModelMap model){
+        model.addAttribute("title", "VTS Drivers");
+        model.addAttribute("menu", "VTS Drivers");
+        return new ModelAndView("iot-devices/vts/drivers/add-driver-additional-info");
+    }
+}
