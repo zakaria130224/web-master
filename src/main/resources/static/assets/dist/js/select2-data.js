ecf7878a294bf020852a546d93054ba110265a1a
shafayet1991 <shafayethossen1991@gmail.com>
2023-08-09 10:34:20 +0600

    UI optimization

--- src/main/resources/static/assets/dist/js/select2-data.js
+++
@@ -1,9 +0,0 @@
-/* Select2 Init*/
-"use strict";
-$(".select2").select2({
-    placeholder: "--Please Select--"
-});
-$("#input_tags").select2({
-    tags: true,
-    tokenSeparators: [',', ' ']
-});
