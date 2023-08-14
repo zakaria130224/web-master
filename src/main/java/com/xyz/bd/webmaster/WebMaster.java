65f0e99ec3ec90973023a919dad8b066d7150500
shafayet1991 <shafayethossen1991@gmail.com>
2023-08-14 18:24:54 +0600

    java 17 enabled

--- src/main/java/com/xyz/bd/webmaster/WebMaster.java
+++ src/main/java/com/xyz/bd/webmaster/WebMaster.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFa
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class WebMaster extends SpringBootServletInitializer {
+public class WebMaster {
 
     public static void main(String[] args) {
         SpringApplication.run(WebMaster.class, args);
